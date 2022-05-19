7f49013c69cc257d696dcfee9873f0c0c1c71639
mmt93 <thiagomatiasmartins@gmail.com>
2022-05-19 17:23:17 -0300

    Output file fixed to rasa's pattern.

--- Chatguy/app.py
+++ Chatguy/app.py
@@ -37,16 +37,44 @@ async def classify_myth(userInput: InputModel):
     try:
         if userInput.texts:
             key = userInput.texts
-            print(key)
             lista = []
             for i in range(len(key)):
-                lista.append(key[i]['suggestions'])
+                if key[i]['entity']:
+                    entity = key[i]['entity']
+                    lista.append(key[i]['suggestions'])
+
             flat_list = [item for sublist in lista for item in sublist]
-            print('este eh o lista: ', flat_list)
             suggest_list = classifier.list_suggesting(key)
-            print(suggest_list)
             result = classifier.phrase_aug(suggest_list, pten_pipeline, enpt_pipeline)
-            print(result)
-            return result
+            list_words = flat_list
+            obj_dict = []
+            entity_keys = ['start','end','value','entity']
+            main_keys = ['text', 'intent', 'entities']
+            values = []
+            intent = userInput.intent
+            texts = list(dict.fromkeys(result))
+            if userInput.isquestion:
+                texts = [w[:-1]+'?' for w in texts]
+
+            for i in range(len(texts)):
+                main_dict = []
+                for word in list_words:
+                    try:
+                        idx = texts[i].find(word)
+                        entities = []
+                        values = []
+                        if idx > -1:
+                            values.extend([idx, idx + len(word) - 1, word, entity])
+                            entities.append(dict(zip(entity_keys, values)))
+                            main_dict.extend([texts[i], intent, entities])
+                            obj_dict.append(dict(zip(main_keys, main_dict)))
+                            break
+                    except:
+                        pass
+
+            json_file = {"rasa_nlu_data":{"regex_features":[],"entity_synonyms":[],"common_examples": []}}
+            json_file['rasa_nlu_data']['common_examples'] = obj_dict
+
+            return json_file
     except Exception as e:
         logger.error("-" + str(e.__class__) + "occurred while running /suggest_sentences/.")
--- Chatguy/models/models.py
+++ Chatguy/models/models.py
@@ -1,7 +1,9 @@
 from pydantic import BaseModel
 
 class InputModel(BaseModel):
+    intent: str
     texts: list
+    isquestion: bool
 
 
 
