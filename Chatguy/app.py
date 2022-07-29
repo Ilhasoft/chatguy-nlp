468a86d407411c1f51341da5744ba7fa392ad3d3
mmt93 <thiagomatiasmartins@gmail.com>
2022-07-29 16:29:06 -0300

    add multiple entities feature; plus bug fix

--- Chatguy/app.py
+++ Chatguy/app.py
@@ -83,23 +83,18 @@ async def suggest_sentences(userInput: InputSentences):
     try:
         if userInput.texts:
             key = userInput.texts
-            lista = []
-            for i in range(len(key)):
-                if not key[i]['entity']:
-                    entity = ['empty']    
-                else:
-                    entity = key[i]['entity']
-                lista.append(key[i]['suggestions'])
-            flat_list = [item for sublist in lista for item in sublist]
-            print(flat_list)
-            if not flat_list:
-                flat_list = ['empty']
-            suggest_list = classifier.list_suggesting(key)
-            print('suggest_list', suggest_list)
-            result = classifier.phrase_gec(suggest_list, model)
 
+            lista_entities = []
+            lista_entities_words = []
+            for i in range(len(key)):
+                if key[i]['entity']:
+                    lista_entities.append([key[i]['entity']])
+                    lista_entities_words.append([key[i]['suggestions']])
+
+            suggest_list = classifier.list_suggesting(key)
+            #result = classifier.phrase_gec(suggest_list, model)
+            result = suggest_list
             # result = classifier.phrase_aug(suggest_list, pten_pipeline, enpt_pipeline)
-            list_words = flat_list
             obj_dict = []
             entity_keys = ['start','end','value','entity']
             main_keys = ['text', 'intent', 'entities']
@@ -107,27 +102,30 @@ async def suggest_sentences(userInput: InputSentences):
             intent = userInput.intent
             texts = list(dict.fromkeys(result))
             if userInput.isquestion:
-                texts = [w[:-1]+'?' for w in texts]
+                texts = [w +' ?' for w in texts]
 
-            for i in range(len(texts)):
-                main_dict = []
-                for word in list_words:
-                    try:
-                        idx = texts[i].find(word)
-                        entities = []
+            for phrase in texts:
+                try:
+                    entities = []
+                    values = []
+                    main_dict = []
+                    for i, entity in enumerate(lista_entities):
+                        lista_words_entity = lista_entities_words[i][0]
                         values = []
-                        if idx > -1:
-                            values.extend([idx, idx + len(word) - 1, word, entity])
-                            entities.append(dict(zip(entity_keys, values)))
-                            main_dict.extend([texts[i], intent, entities])
-                            obj_dict.append(dict(zip(main_keys, main_dict)))
-                            break
-                    except:
-                        pass
+                        for word in lista_words_entity:
+                            idx = phrase.find(word)
+                            if idx > -1:
+                                values.extend([idx, idx + len(word) - 1, word, entity[0]])
+                        entities.append(dict(zip(entity_keys, values)))
+                    main_dict.extend([phrase, intent, entities])
+                    obj_dict.append(dict(zip(main_keys, main_dict)))
+
+                except Exception as e:
+                    logger.error("-" + str(e.__class__) + "occurred while running /suggest_sentences/.")
+        
             json_file = {"rasa_nlu_data":{"regex_features":[],"entity_synonyms":[],"common_examples": []}}
             json_file['rasa_nlu_data']['common_examples'] = obj_dict
 
             return json_file
     except Exception as e:
-        print(str(e.__class__))
         logger.error("-" + str(e.__class__) + "occurred while running /suggest_sentences/.")
