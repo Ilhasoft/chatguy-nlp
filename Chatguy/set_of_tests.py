e06fe969a0f4536a1a567167a009ab504c1446ca
Mel-iza <meliza.caug@gmail.com>
2022-09-30 18:05:14 -0300

    update files

--- Chatguy/set_of_tests.py
+++ Chatguy/set_of_tests.py
@@ -9,4 +9,25 @@ all_tests = [
 "inputs": test_word_generator_function(user_input_word.texts),
 "output": word_synonym_res},
 
+{"Test_sentence_generator": test_sentence_generator_function,
+"inputs": test_word_generator_function(user_input_word.texts),
+"output": word_synonym_res},
+
+{"Test_store_corrections": test_word_generator_function,
+"inputs": test_word_generator_function(user_input_word.texts),
+"output": word_synonym_res},
+
+{"Test_get_synonymous": test_word_generator_function,
+"inputs": test_word_generator_function(user_input_word.texts),
+"output": word_synonym_res},
+
+{"Test_word_generator": test_word_generator_function,
+"inputs": test_word_generator_function(user_input_word.texts),
+"output": word_synonym_res},
+
+{"Test_word_generator": test_word_generator_function,
+"inputs": test_word_generator_function(user_input_word.texts),
+"output": word_synonym_res},
+
+
 ]
