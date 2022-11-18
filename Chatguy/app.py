b5470a5611e849faf8202ffd4dc09942fad3f086
Mel-iza <meliza.caug@gmail.com>
2022-11-18 11:57:32 -0300

    remove postgres adapter from app
    Error from prod:
    File "/home/Chatguy/./app.py", line 58, in <module>
    
        adapter = os.environ['POSTGRES_ADAPTER']
    
      File "/usr/local/lib/python3.8/os.py", line 675, in __getitem__
    
        raise KeyError(key) from None
    
    KeyError: 'POSTGRES_ADAPTER'
    
    INFO:     Stopping reloader process [7]

--- Chatguy/app.py
+++ Chatguy/app.py
@@ -55,8 +55,6 @@ user = os.environ['POSTGRES_USER']
 password = os.environ['POSTGRES_PASSWORD']
 host = os.environ['POSTGRES_HOST']
 port = os.environ['POSTGRES_PORT']
-adapter = os.environ['POSTGRES_ADAPTER']
-
 
 
 DATABASE_URL = f'postgresql://{user}:{password}@{host}:{port}'
